e0c363bcb7f4eb63984bac223d756b33109f9a8f
程遥 <chengyao@cvte.com>
2016-03-13 10:09:07 +0800

    使用nw.js

---
+++ src/elements.js
@@ -0,0 +1,44 @@
+var exports = module.exports = {};
+
+class Bounds {
+    constructor(x, y, height, width, rotation) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+
+        if (typeof(rotation) === 'undefined') rotation = 0;
+        this.rotation = rotation;
+    }
+}
+
+class Element {
+    constructor(id, bounds) {
+        this.id = id;
+        this.bounds = bounds;
+    }
+}
+
+class Shape extends Element {
+    constructor(id, bounds) {
+        super(id, bounds);
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(id, bounds) {
+        super(id, bounds);
+    }
+}
+
+exports.parseSlide = function (xmlString){
+    if (window.DOMParser)
+    {
+        var parser=new DOMParser();
+        var xmlDoc=parser.parseFromString(txt,"text/xml");
+        console.log(xmlDocvar );
+    }
+}
+
+
+
--- src/start.js
+++ src/start.js
@@ -1,17 +1,47 @@
-if(!$){
+if (!$) {
   var $ = require('../lib/jquery.js');
 }
+var model = require('../src/elements.js');
 
-function updateLayout(){
-      var outterElement = $("#main");
-      var container = $("#board-container");      
-      var height = outterElement.height();
-      container.height(height);
+function updateLayout() {
+  var outterElement = $("#main");
+  var container = $("#board-container");
+  container.stop();
+  container.animate({
+    width: outterElement.width(),
+    height: outterElement.height()
+  }, 200);
 }
 
-window.onresize = function(event) {
+window.onresize = function (event) {
   updateLayout();
 };
-$(document).ready(function() {
+
+$(document).ready(function () {
   updateLayout();
+
+  if (require) {
+    load('E:/Code/enbx/Slides/Slide_0.xml')
+  }
 });
+
+function load(file) {
+  var fs = require('fs'),
+    xml2js = require('xml2js');
+
+  fs.readFile(file, function (err, data) {
+    model.parseSlide(data);
+  });
+}
+
+function parseSlide(slide) {
+  var elements = slide.Elements[0];
+  var shapes = elements.Shape;
+  shapes.forEach(function(s) {
+    parseShape(s);
+  }, this);
+}
+
+function parseShape(shape){
+  console.log(shape);
+}
